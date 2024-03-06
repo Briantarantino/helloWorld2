69318007376e83ec537bf1d59d264cbf2b20a3a7
Briantarantino <briant@terpmail.umd.edu>
2024-03-06 17:00:38 -0500

    Updated the app.py sheet to reflect the new about-css html file.

--- app.py
+++ app.py
@@ -17,3 +17,7 @@ if __name__ == '__main__':
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/about-css')
+def about():
+    return render_template('about-css.html')
